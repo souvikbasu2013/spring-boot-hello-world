50dd9a76b3b6ecd0bc35969c061f12324eee9da6
souvikbasu2013 <bunty.nil@gmail.com>
2023-03-11 15:13:56 +0530

    Update HelloWorldController.java
    committed to local main branch.

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -3,11 +3,19 @@ package com.example.helloworld.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.helloworld.model.UserDetails;
+
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
         return "Hello, World!";
+    }
+    
+    @GetMapping("/userbean")
+    public UserDetails helloWorldBean() {
+        UserDetails ud =  new UserDetails("Souvik","Basu",37);
+        return ud;
     }
 }
