7fb7c31152845dc0ec14228ee28fe9e52a26f15e
souvikbasu2013 <bunty.nil@gmail.com>
2023-03-31 00:51:14 +0530

    Update Order.java

--- src/main/java/com/example/helloworld/entities/Order.java
+++ src/main/java/com/example/helloworld/entities/Order.java
@@ -11,18 +11,24 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel(description = "This model is to create an Order")
 @Entity
 @Table(name = "sys_order")
 @Data
 @NoArgsConstructor
 public class Order {
 	
+	@ApiModelProperty(notes = "Auto generated unique id", required = true)
 	@Id
 	@GeneratedValue
 	private Long orderId;
+	
+	@ApiModelProperty(notes = "Order description")
 	@Column(name = "ORDER_DESC")
 	private String orderDescription;
 	
